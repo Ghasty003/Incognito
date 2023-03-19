13e6bc836bfe80890ddbf4f7fe67846aa2d56888
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-19 01:56:08 -0300

    added header

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import Nav from '../components/Nav';
+import header from "../assets/header.jpg";
 
 function Home() {
     return (
-        <div className='custom-bg px-32 py-10 text-white'>
+        <div className='custom-bg px-32 pt-10 pb-28 text-white'>
             <Nav />
-            Home page
+
+            <div className='flex items-center justify-between mt-10'>
+                <div>
+                    <h3 className='capitalize text-6xl'>Stay Incognito and send secret messages.</h3>
+                    <p className='text-lg mt-4'>
+                        Send and receive compliments from friends. You can never know who sent you messages!
+                    </p>
+                </div>
+
+                <img className='w-80 h-80 rounded-lg' src={header} alt="header" />
+            </div>
+
+            <div>
+                <button className='bg-blue-400 text-gray-100 px-8 py-3 rounded-xl active:scale-95 duration-200 hover:drop-shadow-2xl hover:bg-blue-400 hover:text-gray-300'>
+                    Get started
+                </button>
+            </div>
         </div>
     );
 }
