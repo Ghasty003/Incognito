4a052c3638d21d1261931bfe80e206bb1770d1be
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-19 15:23:35 -0300

    implement copy to clipboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { AiOutlineCopy, MdDone } from "react-icons/all";
 import AuthContext from '../contexts/AuthContext';
+
 
 function Dashboard() {
 
     const { user } = React.useContext(AuthContext);
+
+    const [copied, setCopied] = React.useState(false);
+
+    const webUrl = location.host;
+    const userProfile = `${webUrl}/?user=${user?.username}`;
+
+
+    const handleCopy = async () => {
+        const permission = await navigator.permissions.query({ name: "clipboard-write" as PermissionName});
+
+        if (permission.state === "granted") {
+            await navigator.clipboard.writeText(userProfile);
+            setCopied(true);
+
+            setTimeout(() => {
+                setCopied(false);
+            }, 1000);
+        }
+    }
 
     return (
         <div className='flex flex-col items-center'>
@@ -11,11 +32,18 @@ function Dashboard() {
             <div className='bg-navbg w-[400px] text-center p-10 rounded-lg mt-20'>
 
                 <div className='my-5'>
-                    <h1> {user.username}'s Profile</h1>
-                    <p>https://profile.com/ghasty</p>
+                    <h1> {user?.username}'s Profile</h1>
+                    
+                    <div className='flex justify-center gap-2 items-center'>
+                        <p>{ userProfile }</p>
+                        {
+                            copied ? <MdDone color='green' /> :
+                            <AiOutlineCopy title='copy' cursor="pointer" onClick={handleCopy} />
+                        }
+                    </div>
                 </div>
 
-                <p>Share your profile link to get messages from your friend.</p>
+                <p>Share your profile link to get messages from your friends.</p>
 
             </div>
 
