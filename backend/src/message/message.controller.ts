7d4ecd800a89838a9b0f64e90f8deb4304cf0eee
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-19 15:49:05 -0300

    added create message function

--- backend/src/message/message.controller.ts
+++ backend/src/message/message.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { MessageService } from './message.service';
 
 @Controller('message')
-export class MessageController {}
+export class MessageController {
+    constructor (private readonly messageService: MessageService) {}
+
+
+    @Post()
+    async create(@Body() createDto: CreateMessageDto) {
+        return this.messageService.create(createDto);
+    }
+}
