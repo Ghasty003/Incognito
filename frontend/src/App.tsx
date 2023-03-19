e9cd67ce05988eef2d3ba22779794b2ac46aff6f
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-19 05:57:53 -0300

    added dashboard route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Login from "./pages/auth/Login"
 import Register from "./pages/Register"
+import Dashboard from "./pages/Dashboard"
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
     </Routes>
