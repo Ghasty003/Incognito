c5592ba77d30e9e6c903bb665773efcbd141b1b8
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-19 14:03:51 -0300

    Protected route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,22 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
+import { useContext } from "react";
 import Home from "./pages/Home"
 import Login from "./pages/auth/Login"
 import Register from "./pages/auth/Register"
 import Dashboard from "./pages/Dashboard"
+import AuthContext from "./contexts/AuthContext";
+
 
 function App() {
 
+  const { user } = useContext(AuthContext);
+
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/" element={!user ? <Home /> : <Navigate to="/dashboard" />} />
+      <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" />} />
+      <Route path="/register" element={!user ? <Register /> : <Navigate to="/dashboard" />} />
+      <Route path="/login" element={!user ? <Login /> : <Navigate to="/dashboard" />} />
     </Routes>
   )
 }
