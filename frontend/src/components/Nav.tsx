a2a28e382c78387bfc02e9581ec91c12d7065539
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-19 01:24:30 -0300

    updated nav bar

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -20,9 +20,9 @@ function Nav() {
 
     return (
         <nav className='flex justify-between items-center'>
-            <Link to="/" className='flex items-center'>
+            <Link to="/" className='flex items-center gap-1'>
                 <img className='w-12 h-12' src={logo} alt="logo" />
-                <p>Incognito.</p>
+                <h1 className='text-2xl'>Incognito.</h1>
             </Link>
 
             <div className='flex justify-between items-center gap-10'>
