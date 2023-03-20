7898a5709bee3836cb523e4f5c1f6f44bd80aba7
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-19 23:18:02 -0300

    display date

--- frontend/src/components/Message.tsx
+++ frontend/src/components/Message.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { AiFillDelete } from 'react-icons/all';
 import { MessageProp } from '../utils/types';
 
 
-function Message({ message }: MessageProp) {
+function Message({ message, createdAt }: MessageProp) {
+
+    const date = new Date(createdAt);
     
+
     return (
         <div className='bg-primary my-4 p-2 rounded'>
-            { message }
+            <div className='flex justify-between items-center '>
+                <p>{ message }</p>
+                <AiFillDelete />
+            </div>
+
+            { date.getHours() <= 24 ? <p className='text-start'>{ date.getHours() } hrs ago</p> :
+                <p className='text-start'>{ date.toDateString() }</p>
+            }
         </div>
     );
 }
