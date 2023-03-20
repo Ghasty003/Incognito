75d8606c2fb30087952e7e04a5e1c10e32819f35
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-19 23:59:00 -0300

    added send message page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Login from "./pages/auth/Login"
 import Register from "./pages/auth/Register"
 import Dashboard from "./pages/Dashboard"
 import AuthContext from "./contexts/AuthContext";
+import SendMessage from "./pages/SendMessage";
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" />} />
       <Route path="/register" element={!user ? <Register /> : <Navigate to="/dashboard" />} />
       <Route path="/login" element={!user ? <Login /> : <Navigate to="/dashboard" />} />
+      <Route path="/send/:id" element={<SendMessage />} />
     </Routes>
   )
 }
