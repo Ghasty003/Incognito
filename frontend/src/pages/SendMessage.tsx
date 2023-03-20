57bff183bb6a6de4748e7fd347a2b0bcee10f815
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-20 00:28:37 -0300

    updated

--- frontend/src/pages/SendMessage.tsx
+++ frontend/src/pages/SendMessage.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 
 function SendMessage() {
+
+    const query = new URLSearchParams(location.search);
+
+    if (!query.has("user")) {
+        return <Navigate to="/" />
+    }
+
+    const user = query.get("user");
+    // console.log(user);
+ 
     return (
         <div>
             Send message
