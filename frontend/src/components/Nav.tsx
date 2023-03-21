90a8a2320cf49da186f2fae098691db78974af8a
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-21 08:12:52 -0300

    make nav responsive

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { RiMenuFoldLine, RiMenuUnfoldLine } from "react-icons/all";
 import { Link, To, useResolvedPath, useMatch } from 'react-router-dom';
 import logo from "../assets/anonymous-message.png";
 
@@ -24,18 +25,46 @@ function CustomLink({ to, name }: Props) {
 
 function Nav() {
 
+    const [isOpen, setIsOpen] = React.useState(false);
+    const nav = React.useRef<HTMLDivElement>(null!);
+
+    const handleOpen = () => {
+        nav.current.classList.replace("hidden", "flex");
+        setIsOpen(true);
+    }
+
+    const handleClose = () => {
+        nav.current.classList.replace("flex", "hidden");
+        setIsOpen(false);
+    }
+
+
+    React.useEffect(() => {
+        document.addEventListener("click", (e) => {
+            if (!nav.current.classList.contains("hidden") && !(e.target as HTMLElement).classList.contains("flag")) {
+                nav.current.classList.replace("flex", "hidden");
+                setIsOpen(false);
+            }
+        })
+    }, []);
+
     return (
-        <nav className={`flex justify-between px-32 py-6 bg-navbg sticky top-0 items-center }`}>
+        <nav className={`flex justify-between px-12 sm:px-32 py-6 bg-navbg sticky top-0 items-center }`}>
             <Link to="/" className='flex items-center gap-1'>
                 <img className='w-12 h-12' src={logo} alt="logo" />
                 <h1 className='text-2xl'>Incognito.</h1>
             </Link>
 
-            <div className='flex justify-between items-center gap-10'>
+            <div ref={nav} className='sm:flex duration-500  hidden sm:flex-row sm:bg-transparent sm:relative sm:w-fit sm:gap-10 justify-center items-center bg-navbg z-50 h-full fixed top-0 right-0 w-1/2 gap-14 flex-col'>
                 <CustomLink to="/" name='Home'  />
                 <CustomLink to="/login" name='Login' />
                 <CustomLink to="/register" name='Register' />
             </div>
+
+            {
+                isOpen ? <RiMenuUnfoldLine onClick={handleClose} className='cursor-pointer relative z-[100] text-2xl' />  :
+                 <RiMenuFoldLine onClick={handleOpen} className='sm:hidden cursor-pointer text-2xl flag' />
+            }
         </nav>
     );
 }
