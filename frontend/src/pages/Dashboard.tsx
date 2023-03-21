3272f4973f6bf784dad7683e8733eab77a3b2b93
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-21 16:07:10 -0300

    guard user dashboard from unauthorized users

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -10,6 +10,7 @@ function Dashboard() {
     const { user, dispatch: authDispatch } = React.useContext(AuthContext);
 
     const [copied, setCopied] = React.useState(false);
+    const [authorized, setAuthorized] = React.useState(true);
 
     const { messages, dispatch } = React.useContext(MessageContext);
 
@@ -33,13 +34,18 @@ function Dashboard() {
 
     useEffect(() => {
         async function getMessages() {
-            const response = await fetch("http://localhost:3000/message?user=" + user?.username);
+            const response = await fetch("http://localhost:3000/message?user=" + user?.username, {
+                headers: {
+                    Authorization: `Bearer ${user?.token}`
+                }
+            });
 
             const json = await response.json();
 
 
-            if (!response.ok) {
-                console.log("Error", json);
+            if (!response.ok && json.message == "Unauthorized") {
+                setAuthorized(false);
+                localStorage.removeItem("user");
             }
 
             if (response.ok) {
@@ -60,37 +66,43 @@ function Dashboard() {
     return (
         <div className='flex flex-col items-center mb-5'>
 
-            <div className='flex justify-end items-center w-full py-5 px-20'>
-                <button onClick={handleLogout} className='bg-dodger rounded-lg px-4 py-2 drop-shadow-2xl duration-200 active:scale-95'>Logout</button>
-            </div>
+            {
+                authorized ? (
+                    <>
+                        <div className='flex justify-end items-center w-full py-5 px-20'>
+                            <button onClick={handleLogout} className='bg-dodger rounded-lg px-4 py-2 drop-shadow-2xl duration-200 active:scale-95'>Logout</button>
+                        </div>
 
-            <div className='bg-navbg w-[320px] sm:w-[400px] text-center p-5 sm:p-10 rounded-lg mt-20'>
+                        <div className='bg-navbg w-[320px] sm:w-[400px] text-center p-5 sm:p-10 rounded-lg mt-20'>
 
-                <div className='my-5'>
-                    <h1 className='text-xl'> {user?.username}'s Profile</h1>
-                    
-                    <div className='flex justify-center gap-2 items-center'>
-                        <p className='text-sm sm:text-base mt-2'>{ userProfile }</p>
-                        {
-                            copied ? <MdDone color='green' size={20} /> :
-                            <AiOutlineCopy size={20} title='copy' cursor="pointer" onClick={handleCopy} />
-                        }
-                    </div>
-                </div>
+                            <div className='my-5'>
+                                <h1 className='text-xl'> {user?.username}'s Profile</h1>
+                                
+                                <div className='flex justify-center gap-2 items-center'>
+                                    <p className='text-sm sm:text-base mt-2'>{ userProfile }</p>
+                                    {
+                                        copied ? <MdDone color='green' size={20} /> :
+                                        <AiOutlineCopy size={20} title='copy' cursor="pointer" onClick={handleCopy} />
+                                    }
+                                </div>
+                            </div>
 
-                <p>Share your profile link to get messages from your friends.</p>
+                            <p>Share your profile link to get messages from your friends.</p>
 
-            </div>
+                        </div>
 
-            <div className='bg-navbg w-[320px] sm:w-[400px] text-center p-10 rounded-lg mt-20'>
-                <h2>Messages ({ messages.length })</h2>
+                        <div className='bg-navbg w-[320px] sm:w-[400px] text-center p-10 rounded-lg mt-20'>
+                            <h2>Messages ({ messages.length })</h2>
 
-                {
-                    messages.length > 0 ? messages.map((message) => (
-                        <Message key={message._id} _id={message._id} message={message.message} createdAt={message.createdAt} />
-                    )) : <p className='italic mt-3'>You have no message at the moment.</p>
-                }
-            </div>
+                            {
+                                messages.length > 0 ? messages.map((message) => (
+                                    <Message key={message._id} _id={message._id} message={message.message} createdAt={message.createdAt} />
+                                )) : <p className='italic mt-3'>You have no message at the moment.</p>
+                            }
+                        </div>
+                    </>
+                ) : <h2 className='text-3xl mt-[20%]'>Unauthorized User.</h2>
+            }
 
         </div>
     );
