14e3e458180cf5259aee87c604279c7d4fe18c30
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-21 00:18:09 -0300

    completed layout

--- frontend/src/pages/SendMessage.tsx
+++ frontend/src/pages/SendMessage.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import Nav from '../components/Nav';
-import { BsSend } from "react-icons/all";
+import { BsSend, MdDone } from "react-icons/all";
 import img404 from "../assets/panda-404.png";
+import MessageContext from '../contexts/MessageContext';
 
 function SendMessage() {
 
@@ -10,12 +11,17 @@ function SendMessage() {
 
     const [exist, setExist] = useState(false);
     const [message, setMessage] = useState("");
+    const [isSent, setIsSent] = useState(false);
+    const [err, setErr] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
     if (!query.has("user")) {
         return <Navigate to="/" />
     }
 
     const user = query.get("user");
+
+    const { dispatch } = useContext(MessageContext);
 
     useEffect(() => {
         async function getUser() {
@@ -33,6 +39,7 @@ function SendMessage() {
     const handleSubmit = async (e: React.FormEvent) => {
 
         e.preventDefault();
+        setIsLoading(true);
 
         const res = await fetch("http://localhost:3000/message", {
             method: "POST",
@@ -45,11 +52,25 @@ function SendMessage() {
         const json = await res.json();
 
         if (!res.ok) {
-            console.log("Error", json);
+            setErr(json.message);
+
+            setTimeout(() => {
+                setErr("");
+            }, 2000);
+
+            setIsLoading(false);
         }
 
         if (res.ok) {
-            console.log(json)
+            dispatch({ type: "ADD_MESSAGE", payload: json});
+            setIsLoading(false);
+            setIsSent(true);
+
+            setMessage("");
+
+            setTimeout(() => {
+                setIsSent(false);
+            }, 2000);
         }
     }
  
@@ -61,17 +82,28 @@ function SendMessage() {
                 exist ? (
                     <div className='flex flex-wrap justify-around items-center translate-y-1/2 px-32'>
 
-                        <div className='bg-navbg text-center sm:mt-0 mt-8 sm:w-96 h-[250px] flex flex-col rounded-lg p-6'>
+                        <div className='bg-navbg text-center sm:mt-0 mt-8 sm:w-96 h-[280px] flex flex-col rounded-lg p-6'>
 
                             <h2 className='text-xl'>Send a secret message to { user }</h2>
 
                             <form onSubmit={handleSubmit} className='mt-6'>
-                                <input value={message} onChange={(e) => setMessage(e.target.value)} type="text" className='border-none outline-none caret-dodger bg-primary w-60 pb-20 rounded' />
+                                <input value={message} onChange={(e) => setMessage(e.target.value)} type="text" className='border-none outline-none caret-dodger bg-primary px-1 py-1 w-60 pb-20 rounded' />
                                 <br />
 
-                                <button className='flex items-center justify-center gap-1 bg-dodger px-6 py-2 rounded-lg mt-4 relative left-1/2 -translate-x-1/2'>
-                                <BsSend /> Send
-                                </button>
+                                {
+                                    err && <p className='text-red-500 mt-2'>{ err }.</p>
+                                }
+
+                                {
+                                    isSent ? <p className='flex justify-center items-center mt-4 gap-1'>Message sent <MdDone color='green' size={20} /></p> : (
+                                        <button disabled={isLoading} className='flex items-center justify-center gap-1 bg-dodger px-6 py-2 rounded-lg mt-4 relative left-1/2 -translate-x-1/2'>
+                                            {
+                                                isLoading ? <div className='animate-spin w-5 h-5 border-[2px] border-gray-600 rounded-full border-t-black mr-1'></div> : ""
+                                            }
+                                            <BsSend /> Send
+                                        </button>
+                                    )
+                                }
                             </form>
                         </div>
                 
