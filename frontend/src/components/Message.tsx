220a67396a27fcf1164dad6226e396b2dca699bf
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-20 23:07:44 -0300

    added delete message functionality

--- frontend/src/components/Message.tsx
+++ frontend/src/components/Message.tsx
@@ -3,13 +3,30 @@ import { AiFillDelete } from 'react-icons/all';
 import { MessageProp } from '../utils/types';
 
 
-function Message({ message, createdAt }: MessageProp) {
+function Message({ message, createdAt, _id }: MessageProp) {
 
     const date = new Date(createdAt);
+
+
+    const handleClick = async () => {
+        const res = await fetch("http://localhost:3000/message/" + _id, {
+            method: "DELETE"
+        });
+
+        const json = await res.json();
+
+        if (!res.ok) {
+            console.log(json);
+        }
+
+        if (res.ok) {
+            console.log(json);
+        }
+    }
     
 
     return (
-        <div className='bg-primary my-4 p-2 rounded'>
+        <div onClick={handleClick} className='bg-primary my-4 p-2 rounded'>
             <div className='flex justify-between items-center '>
                 <p>{ message }</p>
                 <AiFillDelete className='text-red-600 text-xl cursor-pointer' />
