e8f62366016fa6b02f194bf60d0668760432b49d
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-04-14 15:00:57 -0300

    updated loading component

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 function Loading() {
     return (
-        <div>
-            Loading...
+        <div className='h-screen flex items-center justify-center gap-2'>
+            <div className='animate-spin w-8 h-8 border-[4px] border-white rounded-full border-t-dodger mr-1' />
+            <p className='italic text-xl'>Loading...</p>
         </div>
     );
 }
