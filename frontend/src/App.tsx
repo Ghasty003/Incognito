edc67631d2f6da73f886a8c8a07f2426115a5980
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-04-14 13:59:14 -0300

    implement lazy loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,45 @@
 import { Navigate, Route, Routes } from "react-router-dom"
-import { useContext } from "react";
-import Home from "./pages/Home"
-import Login from "./pages/auth/Login"
-import Register from "./pages/auth/Register"
-import Dashboard from "./pages/Dashboard"
+import { useContext, Suspense, lazy } from "react";
 import AuthContext from "./contexts/AuthContext";
-import SendMessage from "./pages/SendMessage";
+import Home from "./pages/Home"
 
 
 function App() {
 
   const { user } = useContext(AuthContext);
 
+  const Dashboard = lazy(() => import("./pages/Dashboard"));
+  const SendMessage = lazy(() => import("./pages/SendMessage"));
+  const Register = lazy(() => import("./pages/auth/Register"));
+  const Login = lazy(() => import("./pages/auth/Login"));
   
   return (
     <Routes>
-      <Route path="/" element={!user ? <Home /> : <Navigate to="/dashboard" />} />
-      <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" />} />
-      <Route path="/register" element={!user ? <Register /> : <Navigate to="/dashboard" />} />
-      <Route path="/login" element={!user ? <Login /> : <Navigate to="/dashboard" />} />
-      <Route path="/send" element={<SendMessage />} />
+      <Route index path="/" element={!user ? <Home /> : <Navigate to="/dashboard" />} />
+
+      <Route path="/dashboard" element={user ? 
+        <Suspense fallback={<h1>Loading..</h1>}>
+          <Dashboard />
+        </Suspense> : <Navigate to="/login" />} 
+      />
+
+      <Route path="/register" element={!user ? 
+        <Suspense fallback={<h1>Loading..</h1>}>
+          <Register />
+        </Suspense> : <Navigate to="/dashboard" />} 
+      />
+
+      <Route path="/login" element={!user ? 
+        <Suspense fallback={<h1>Loading..</h1>}>
+          <Login />
+        </Suspense> : <Navigate to="/dashboard" />} 
+      />
+
+      <Route path="/send" element={
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <SendMessage />
+        </Suspense>
+      } />
     </Routes>
   )
 }
